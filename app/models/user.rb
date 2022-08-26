85d1db9ef48b4a6828d2ec1a4c54e49ee1aff115
03mayank <mdubey@grepruby.io>
2022-08-26 19:25:35 +0530

    User Authentication commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,7 @@ class User < ApplicationRecord
   has_secure_password
 
   validates :email, presence: true, uniqueness: true, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: 'Invalid email' }
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :mobile, presence: true
 end
