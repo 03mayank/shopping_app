6c548a9fb7f771f4a11866ef4c2ea404bc30e50d
03mayank <mdubey@grepruby.io>
2022-10-07 11:08:50 +0530

    Devise Integration

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,22 +1,6 @@
 class RegistrationsController < ApplicationController
-  def new
-    @user = User.new
-  end
   
-  def create
-    @user = User.new(user_params)
-    if @user.save
-      session[:user_id] = @user.id
-      redirect_to root_path, notice: "Signed In"
-      # SignupMailer.with(user: @user).new_user_registration_email.deliver_later
-      SignupJob.perform_later(user: @user)
-    else
-      render :new, status: :unprocessable_entity
-    end
-  end
-
-  private
-  def user_params
-    params.require(:user).permit(:email, :password, :password_confirmation, :first_name, :last_name, :mobile)
+  def index
+    redirect_to new_user_session_path
   end
 end
