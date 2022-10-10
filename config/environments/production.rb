87f07116ee5c8f034a6984d2022604d59181b529
03mayank <110469010+03mayank@users.noreply.github.com>
2022-10-10 15:33:38 +0530

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -122,8 +122,8 @@ Rails.application.configure do
     address:              'smtp.gmail.com',
     port:                 587,
     domain:               'gmail.com',
-    user_name:            'mdubey@grepruby.io',
-    password:             'ovkevioesvrbcetp',
+    user_name:            '',
+    password:             '',
     authentication:       :plain,
     enable_starttls_auto: true
   }
